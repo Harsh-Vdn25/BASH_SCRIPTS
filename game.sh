74875004b3872e30751a26c05e8453a0cc82d090
Harsh-Vdn25 <hars.vdn16@gmail.com>
2025-12-14 23:08:31 +0530

    Working

--- game.sh
+++ game.sh
@@ -6,54 +6,61 @@ USER_A=0
 USER_B=0
 CHANCE=0
 GAME_FINISHED=0
+WINNER=""
 function rollTheDie(){
 	VALUE=$(shuf -i 1-100 -n 1)
 	VALUE=$(echo "scale=2; $VALUE / 100" | bc)
 	DECIMAL=$(echo " scale=2; $VALUE * 6" |bc)
-	RESULT=$(awk '{print(x == int($1)?$1:int($1)+1)}' <<<"$DECIMAL")
+	RESULT=$(awk '{print($1 == int($1)?int($1):int($1)+1)}' <<<"$DECIMAL")
 	echo $RESULT
 }
 function PlayGame(){
 	while [ $GAME_FINISHED -eq 0 ]; do 
 		if [ $CHANCE -eq 0 ]; then
-			echo "User 1 should roll the die"
+			echo -e "User 1 should roll the die\n"
 			read -p "please roll the die:" USER_INPUT
 			if [ $USER_INPUT -eq 1 ];then
-				DIE=$rollTheDie
-				echo "die value ra  pooka :$rollTheDie user input ikkada:$USER_INPUT"
+				DIE=$(rollTheDie)
+				echo -e "=== User1 die value $DIE ===\n"
 				if [ $(($DIE + $USER_A)) -gt $RACE_LENGTH ];then
 					echo -e "\nYou cant advance ,you need exact $(($RACE_LENGTH - $USER_A)) to advance"
 				else
 					USER_A=$((USER_A + DIE))
-					CHANCE=1
 				fi
+				CHANCE=1
 			else
 				echo -e "\nYou can't advance so try again "
 			fi
 		else
-			echo "User 2 should roll the die" 
+			echo -e "User 2 should roll the die\n" 
 			read -p "please roll the die:" USER_INPUT
 			if [ $USER_INPUT -eq 1 ];then
-				DIE=$rollTheDie
-				echo "$DIE $USER_INPUT"
+				DIE=$(rollTheDie)
+				echo -e "=== User2 die value $DIE ===\n"
 				if [ $(($DIE + $USER_B)) -gt $RACE_LENGTH ];then
 					echo -e "\nYou cant advance ,you need exact $(($RACE_LENGTH - $USER_B)) to advance"
 				else
 					USER_B=$((USER_B + DIE))
-					CHANCE=0
 				fi
+				CHANCE=0
 			else
 				echo "You can't advance so try again "
 			fi
 		fi
 		if [[ $USER_A -eq 30 || $USER_B -eq 30 ]]; then
 			GAME_FINISHED=1
-			((WINNER = $USER_A > $USER_B ? "USER_A" : "USER_B"))
+			if [ $USER_A > $USER_B ];then
+				WINNER="USER_A"
+			else
+				WINNER="USER_B"
+			fi
 			echo "$WINNER won the game"
 		else
-			echo -e "\n  =========SCORES========="
-			echo -e "\n $USER_A | $USER_B \n"
+			echo -e "  =========SCORES========="
+			echo -e "\n 		$USER_A | $USER_B "
+			echo -e "\n  ========================\n"
 		fi
 		done
 }
 PlayGame
+
